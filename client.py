7639ca3caf38a83a612182108a53231b52cb05d8
letrmkhoi1411 <114128806+letrmkhoi1411@users.noreply.github.com>
2023-12-01 03:16:40 -0800

    Update client.py

--- client.py
+++ client.py
@@ -1,37 +1,96 @@
-from socket import *
+import socket
+import pickle
 
+# import the game
+from tictactoe import TicTacToe
 
-# Function to start the client
-def start_client():
-    serverName = gethostbyname("localhost")
-    serverPort = 12000
-    client_socket = socket(AF_INET, SOCK_STREAM)
-    client_socket.connect((serverName, serverPort))
+HOST = '127.0.0.1'  # the server's IP address 
+PORT = 12000        # the port we're connecting to 
 
-    # Play the game
-    play_game(client_socket)
+# connect to the host
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((HOST, PORT))
+print(f"\nConnected to {client_socket.getsockname()}!")
 
-# Function for the client to play the Tic Tac Toe game
-def play_game(client_socket):
-    while True:
-        # Receive and display the current board
-        board = client_socket.recv(1024).decode()
-        print(board)
+# set up the game
+player_B = TicTacToe("B")
 
-        # Make a move
-        move = input("Enter your move (0-8): ")
-        client_socket.send(move.encode())
+# allow the player to suggest playing again
+rematch = True
 
-        # Check for game over
-        message = client_socket.recv(1024).decode()
-        if message:
-            print(message)
-        
-        if "win" in message or "tie" in message:
+while rematch == True:
+    # a header for an intense tic-tac-toe match! 
+    print(f"\n\n T I C - T A C - T O E ")
+
+    # draw the grid
+    player_B.display_board()
+
+    # host goes first, client receives first
+    print(f"\nWaiting for other player...")
+    A_board = client_socket.recv(1024)
+    A_board = pickle.loads(A_board)
+    player_B.update_board(A_board)
+
+    # the rest is in a loop; if either player has won, it exits 
+    while player_B.check_win("A") == False and player_B.check_win("B") == False and player_B.is_draw() == False:
+        # draw grid, ask for coordinate 
+        print(f"\nYour turn!")
+        player_B.display_board()
+        move = input(f"Enter your move (0-8): ")
+        player_B.edit_board(move)
+
+        # draw grid again
+        player_B.display_board()
+
+        B_board = pickle.dumps(player_B.board)
+        client_socket.send(B_board)
+
+        # if the player won with the last move or it's a draw, exit the loop 
+        if player_B.check_win("B") == True or player_B.is_draw() == True:
             break
 
-    # Close the connection
-    client_socket.close()
+        # wait to receive the symbol list and update it
+        print(f"\nWaiting for other player...")
+        A_board = client_socket.recv(1024)
+        A_board = pickle.loads(A_board)
+        player_B.update_board(A_board)
 
-if __name__ == "__main__":
-    start_client()
+    # end game messages
+    if player_B.check_win("B") == True:
+        print(f"Congrats, you won!")
+    elif player_B.is_draw() == True:
+        print(f"It's a draw!")
+    else:
+        print(f"the A won.")
+
+    # host is being asked for a rematch, awaiting response 
+    print(f"\nWaiting for host...")
+    host_response = client_socket.recv(1024).decode()
+    client_response = "N"
+
+    # if the host wants a rematch, then the client is asked 
+    if host_response == "Y":
+        print(f"\nThe host would like a rematch!")
+        client_response = input("Rematch? (Y/N): ")
+        client_response = client_response.capitalize()
+        temp_client_resp = client_response
+
+        # let the host know what the client decided 
+        client_socket.send(client_response.encode())
+
+        # if the client wants a rematch, restart the game
+        if temp_client_resp == "Y":
+            player_B.restart()
+
+        # if the client said no, then no rematch 
+        else:
+            rematch = False
+
+    # if the host said no, then no rematch 
+    else:
+        print(f"\nThe host does not want a rematch.")
+        rematch = False
+
+spacer = input(f"\nPress enter to quit...\n")
+
+client_socket.close()
